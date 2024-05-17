ae624e5a264d38d8618788df83bef4f6eb1f893f
Timmy <kouadiobhegnino@gmail.com>
2024-05-17 14:19:07 +0000

    encode_cert_path

--- src/assets/credentials.ts
+++ src/assets/credentials.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 
-const privateKey = fs.readFileSync("./server.key");
-const certificate = fs.readFileSync("./server.pem");
+const privateKey = fs.readFileSync('./server.key', {encoding: 'utf8', flag: 'r'});
+const certificate = fs.readFileSync('./server.pem', {encoding: 'utf8', flag: 'r'});
 const credentials = {key: privateKey, cert: certificate};
 
 export default credentials
